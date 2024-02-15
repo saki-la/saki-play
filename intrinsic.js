3fa6e4a4945497a6c596da45c6c62e7f5d10f035
saki-la <157243597+saki-la@users.noreply.github.com>
2024-02-15 14:32:53 -0800

    Update intrinsic.js

--- intrinsic.js
+++ intrinsic.js
@@ -6,5 +6,7 @@ export const intrinsic = {
   "and": ["Cxy",2,["I"],["var","F"]],  // a|b| a b F
   "or": ["Cxy",1,["I"],["var","T"]],   // a|b| a T b
   // xor = a|b| a (not b) b
-  "xor": ["Cxy",1,["Bxy",1,["S",1],["Cxy",1,["B",1],["var","not"]]],["I"]]
+  "xor": ["Cxy",1,["Bxy",1,["S",1],["Cxy",1,["B",1],["var","not"]]],["I"]],
+  // sub1 = x|y|b| x (s| s (y b; not b); y b F) (s| s (y (not b) b); y T b)
+  "sub1": ["Cxy",1,["Bxy",1,["S",2],["Cxy",1,["B",2],["Sxy",2,["Bxy",2,["C",1],["Bxy",2,["Cx",1,["I"]],["Cxy",1,["S",1],["var","not"]]]],["Cxy",2,["I"],["var","F"]]]]],["Sxy",2,["Bxy",2,["C",1],["Bxy",2,["Cx",1,["I"]],["Cxy",1,["Bxy",1,["S",1],["Cxy",1,["B",1],["var","not"]]],["I"]]]],["Cxy",1,["I"],["var","T"]]]]
 };
