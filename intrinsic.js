00405e718655a6b516d6fbc48b5811e4512eb52f
saki-la <157243597+saki-la@users.noreply.github.com>
2024-02-15 13:36:04 -0800

    Update intrinsic.js

--- intrinsic.js
+++ intrinsic.js
@@ -1,1 +1,10 @@
-
+export const intrinsic = {
+  "Y": ["Bxy",1,["Sxy",1,["I"],["I"]],["Cxy",1,["B",1],["Sxy",1,["I"],["I"]]]],
+  "F": ["Kx",1,["I"]],                 // x|y| y 
+  "T": ["K",1],                        // x|y| x
+  "not": ["C",1],                      // a|x|y| a y x
+  "and": ["Cxy",2,["I"],["var","F"]],  // a|b| a b F
+  "or": ["Cxy",1,["I"],["var","T"]],   // a|b| a T b
+  // xor = a|b| a (not b) b
+  "xor": ["Cxy",1,["Bxy",1,["S",1],["Cxy",1,["B",1],["var","not"]]],["I"]]
+};
