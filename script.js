073757a9d70e6cae42280dfe35575fbf1b0e1b33
saki-la <157243597+saki-la@users.noreply.github.com>
2024-03-22 15:40:10 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
+// code| replLam; LXPtoLCX code
+
+// forceLXP = true
+// code: x|y|b| x (y b iy j k) b  // NG
+// code: x|y|b| x (y b yi j k) b  // OK
+/*
+((C2 ((B2 S)((C B)((C2 ((C2 (C2 I yi)))))))))
+["LCX:app", [
+"LCX:app",["LCX:C",2],["LCX:app",["LCX:app",["LCX:B",2],["LCX:S",1],false],["LCX:app",["LCX:app",["LCX:C",1],["LCX:B",2],false],["LCX:app",["LCX:app",["LCX:C",2],["LCX:app",["LCX:app",["LCX:C",2],["LCX:app",["LCX:app",["LCX:C",2],["LCX:I"],false],["LCX:var","yi"],["yi"]],["yi"]],["LCX:var","j"],["j","yi"]],["j","yi"]],["LCX:var","k"],["j","k","yi"]],["j","k","yi"]],["j","k","yi"]],["j","k","yi"]],["LCX:I"],["j","k","yi"]]
+*/
+
+
 "use strict";
-import { intrinsic } from "https://saki-la.github.io/saki-play/intrinsic.js";
+import { intrSkip, intrinsic } from "https://saki-la.github.io/saki-play/intrinsic.js";
 import { parse } from "https://saki-la.github.io/saki-play/sakiMin.pegjs.js";
 import { pegToLXP } from "https://saki-la.github.io/saki-play/PEGToLXP.js";
 import { LXPtoCXP } from "https://saki-la.github.io/saki-play/LXPtoCXP.js";
 import {
-  setLibrary,
-  CXPtoXRF,            // convert CXP to XRF
-  reduceXRF,           // evaluation main
-  XRFtoCXP, XRFtoJSON  // how to get the result
+  setLibrary, setDebugSkip,
+  CXPtoXRF,             // convert CXP to XRF
+  reduceXRF,            // evaluation main
+  XRFtoCXP, XRFtoJSON,  // how to get the result
+  XRFtoComb, XRFtoStr,
+  setOutputJSON, setOutputComb, setOutputStr
 } from "https://saki-la.github.io/saki-play/eval.js";
 
 setLibrary(Object.assign(intrinsic, {  // library in eval.js
-
+  "BnComb1": ["Kx",1,["Bxy",1,["K",3],["Bxy",2,["K",8],["Cx",1,["Cxy",1,["Cxy",1,["I"],["Kx",9,["I"]]],["Kx",9,["I"]]]]]]],
+  "debug": ["I"]
 }));
 
 let outputJSON = true;  // output in JSON representation
@@ -19,70 +34,69 @@ let outputComb = true;  // output in combinator form
 let outputStr = true;   // output as string (no escapes)
 let compact = true;  // compacted JSON (no CRs or spaces)
 let debugState = void 0;  // debug mode if not undefined
+const debug1 = "V1";
+const debug2 = "V2";
+//const debug1 = "D1";
+//const debug2 = "D2";
+setDebugSkip(intrSkip);
 
 let forceLXP = false;
+const enableMyLXPtoCXP = false;
 
-const AryToStr = (json) => {
-  if (
-    outputStr &&
-    toString.call(json) == "[object Array]" &&
-    json.reduce((a, e) => a && typeof e == "number", true)
-  ) {
-    const u8a = new Uint8Array(json);
-    return new TextDecoder().decode(u8a); // UTF-8
+const LXPStrToExpr = (lxp) => ({
+  // (lam|app|var|js|ph| lam v x) = (B K4; C (C I v) x)
+  "lam": () => [  // lambda
+    "Bxy", 1, ["K", 4], [
+      "Cxy", 1, [
+        "Cxy", 1, ["I"], ["+", lxp[1]]  // v
+      ], LXPStrToExpr(lxp[2])  // x
+    ]
+  ],
+  // (lam|app|var|js|ph| app x y) = (K; B K3; C (C I x) y)
+  "app": () => [  // application
+    "Kx", 1, [
+      "Bxy", 1, ["K", 3], [
+        "Cxy", 1, [
+          "Cxy", 1, ["I"], LXPStrToExpr(lxp[1])  // x
+        ], LXPStrToExpr(lxp[2])  // y
+      ]
+    ]
+  ],
+  // (lam|app|var|js|ph| var v) = (K2; B K2; C I v)
+  "var": () => [  // variable
+    "Kx", 2, [
+      "Bxy", 1, ["K", 2], [
+        "Cxy", 1, ["I"], ["+", lxp[1]]  // v
+      ]
+    ]
+  ],
+  // (lam|app|var|js|ph| js j) = (K3; B K; C I j)
+  "+": () => [  // JSON value (boolean, number, string, array)
+    "Kx", 3, [
+      "Bxy", 1, ["K", 1], [
+        "Cxy", 1, ["I"], ["+", lxp[1]]  // j
+      ]
+    ]
+  ],
+  // (lam|app|var|js|ph| ph) = (K4 I)
+  "()": () => ["Kx", 4, ["I"]]
+}[lxp[0]]());  // LXPStrToExpr
+const MyLXPtoCXP = (lxp) => {
+  if (enableMyLXPtoCXP && forceLXP) {
+    const xrf = CXPtoXRF([
+      "app", [
+        "var", "LXPtoCXP"
+      ], LXPStrToExpr(lxp)
+    ]);
+    const [, [sc, mc, ,]] = reduceXRF(xrf, ["OK", "ND", 1000000, []]);
+    if (sc != "ER") 
+      return XRFtoJSON(xrf);
+    else
+      return ["+", "LXPtoCXP: " + sc + " " + mc];
   } else {
-    return json;
+    return LXPtoCXP(lxp);
   }
-};
-const NtoStr = (n) => (n == 1 ? "" : "" + n);
-const CXPtoStr = (cxp) =>
-  ({
-    Sxy: () => "(S" + NtoStr(cxp[1]) + " " + CXPtoStr(cxp[2]) + " " +
-                                             CXPtoStr(cxp[3]) + ")",
-    Sx: () => "(S" + NtoStr(cxp[1]) + " " + CXPtoStr(cxp[2]) + ")",
-    S: () => "S" + NtoStr(cxp[1]),
-    Kx: () => "(K" + NtoStr(cxp[1]) + " " + CXPtoStr(cxp[2]) + ")",
-    K: () => "K" + NtoStr(cxp[1]),
-    Cxy: () =>
-      "(C" +
-      NtoStr(cxp[1]) +
-      " " +
-      CXPtoStr(cxp[2]) +
-      " " +
-      CXPtoStr(cxp[3]) +
-      ")",
-    Cx: () => "(C" + NtoStr(cxp[1]) + " " + CXPtoStr(cxp[2]) + ")",
-    C: () => "C" + NtoStr(cxp[1]),
-    Bxy: () =>
-      "(B" +
-      NtoStr(cxp[1]) +
-      " " +
-      CXPtoStr(cxp[2]) +
-      " " +
-      CXPtoStr(cxp[3]) +
-      ")",
-    Bx: () => "(B" + NtoStr(cxp[1]) + " " + CXPtoStr(cxp[2]) + ")",
-    B: () => "B" + NtoStr(cxp[1]),
-    I: () => "I",
-    app: () => "(" + CXPtoStr(cxp[1]) + " " + CXPtoStr(cxp[2]) + ")",
-    var: () => cxp[1],
-    "()": () => "()",
-    "+": () => JSON.stringify(cxp[1])
-  }[cxp[0]]());
-const XRFtoComb = (xrf) => {  // stringify XRF in combinator form
-  if (outputComb) {
-    return CXPtoStr(XRFtoCXP(xrf));
-  } else {
-    return XRFtoCXP(xrf);
-  }
-};
-const XRFtoStr = (xrf) => {  // stringify XRF in JSON
-  if (outputJSON) {
-    const json = AryToStr(XRFtoJSON(xrf));
-    if (json !== void 0) return json;
-  }
-  return XRFtoComb(xrf);
-};
+}
 
 const nodeToText = (nd) => {
   switch (nd.nodeName.toLowerCase()) {
@@ -107,7 +121,6 @@ const nodesToText = (ns) => {
   });
   return t;
 };
-
 const pastePlainText = (e) => {
   let text = e.clipboardData.getData("text/plain");
   const selection = window.getSelection();
@@ -160,11 +173,16 @@ const pastePlainText = (e) => {
   }
   selection.setPosition(nodeIns, offsetIns);
   e.preventDefault();
-};
+};  // pastePlainText
 
 document.addEventListener("DOMContentLoaded", () => {
   const elemCode = document.getElementById("code");
-  elemCode.textContent = ""; //"         1         2         3         4         5         6\n123456789012345678901234567890123456789012345678901234567890";
+  if (enableMyLXPtoCXP) {
+    elemCode.textContent = "";
+  } else {
+    elemCode.textContent = "code| replLam; LXPtoLCX code"; //"         1         2         3         4         5         6\n123456789012345678901234567890123456789012345678901234567890";
+  }
+  
   elemCode.addEventListener("paste", pastePlainText);
   const elemInput = document.getElementById("input");
   elemInput.textContent = "";
@@ -181,44 +199,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const elemNext = document.getElementById("reduceNext");
   const elemReset = document.getElementById("reduceReset");
   const elemCopy = document.getElementById("copyToClipboard");
-  const LXPStrToExpr = (lxp) => ({
-    // (lam|app|var|js|ph| lam v x) = (B K4; C (C I v) x)
-    "lam": () => [  // lambda
-      "Bxy", 1, ["K", 4], [
-        "Cxy", 1, [
-          "Cxy", 1, ["I"], ["+", lxp[1]]  // v
-        ], LXPStrToExpr(lxp[2])  // x
-      ]
-    ],
-    // (lam|app|var|js|ph| app x y) = (K; B K3; C (C I x) y)
-    "app": () => [  // application
-      "Kx", 1, [
-        "Bxy", 1, ["K", 3], [
-          "Cxy", 1, [
-            "Cxy", 1, ["I"], LXPStrToExpr(lxp[1])  // x
-          ], LXPStrToExpr(lxp[2])  // y
-        ]
-      ]
-    ],
-    // (lam|app|var|js|ph| var v) = (K2; B K2; C I v)
-    "var": () => [  // variable
-      "Kx", 2, [
-        "Bxy", 1, ["K", 2], [
-          "Cxy", 1, ["I"], ["+", lxp[1]]  // v
-        ]
-      ]
-    ],
-    // (lam|app|var|js|ph| js j) = (K3; B K; C I j)
-    "+": () => [  // JSON value (boolean, number, string, array)
-      "Kx", 3, [
-        "Bxy", 1, ["K", 1], [
-          "Cxy", 1, ["I"], ["+", lxp[1]]  // j
-        ]
-      ]
-    ],
-    // (lam|app|var|js|ph| ph) = (K4 I)
-    "()": () => ["Kx", 4, ["I"]]
-  }[lxp[0]]());
+
   const updateOutput = (debug = false) => {
     let data;
     const code = nodesToText(elemCode.childNodes);
@@ -231,7 +212,7 @@ document.addEventListener("DOMContentLoaded", () => {
         data = ["+", JSON.parse(code)];
       } catch {
         try {
-          data = LXPtoCXP(pegToLXP(parse(code)));
+          data = MyLXPtoCXP(pegToLXP(parse(code)));
         } catch {
           data = void 0;  // ignore input errors
         }
@@ -240,9 +221,11 @@ document.addEventListener("DOMContentLoaded", () => {
         input = ["+", JSON.parse(inputElem)];
       } catch {
         try {
-          if (forceLXP) {
+          if (!enableMyLXPtoCXP && forceLXP) {
             input = LXPStrToExpr(pegToLXP(parse(inputElem)));
-          } else input = LXPtoCXP(pegToLXP(parse(inputElem)));
+          } else {
+            input = MyLXPtoCXP(pegToLXP(parse(inputElem)));
+          }
           if (input[0] == "()") input = void 0;
         } catch {
           input = void 0;  // ignore input errors
@@ -252,7 +235,7 @@ document.addEventListener("DOMContentLoaded", () => {
         input2 = ["+", JSON.parse(input2Elem)];
       } catch {
         try {
-          input2 = LXPtoCXP(pegToLXP(parse(input2Elem)));
+          input2 = MyLXPtoCXP(pegToLXP(parse(input2Elem)));
           if (input2[0] == "()") input2 = void 0;
         } catch {
           input2 = void 0;  // ignore input errors
@@ -272,18 +255,22 @@ document.addEventListener("DOMContentLoaded", () => {
               : ["app", CXPtoXRF(data), CXPtoXRF(input), void 0]
             : CXPtoXRF(data);  // no input if input is not given
         if (debug) {
-          debugState = ["OK", "D1", 10000, [xrf]];  // enter debug mode
+          debugState = ["OK", debug1, 1000000, [xrf]];  // enter debug mode
           elemOutputResult.textContent = "Start debugging";
           data = XRFtoComb(xrf);
         } else {
-          const [, [sc, mc, ,]] = reduceXRF(xrf);  // reduce external code (subject to debugging)
+          const [, [sc, mc, ,]] = reduceXRF(xrf, ["OK", "ND", 1000000, []]);  // reduce external code (subject to debugging)
           elemOutputResult.textContent = sc + " " + mc;
-          data = XRFtoStr(xrf);
+          if (sc != "ER" || mc != "EC") 
+            data = XRFtoStr(xrf);
+          else
+            data = "N/A";
         }
         //} catch {
         //  data = void 0;
         //  nextCXP = void 0;
         //}
+        data = data.slice(0, 10000);  // cut it short
       }
     } else {  // debugState !== void 0
       //try {
@@ -291,9 +278,9 @@ document.addEventListener("DOMContentLoaded", () => {
       const [xrfNew, [sc, mc, cnt, apps]] = reduceXRF(xrf, debugState);  // reduce external code (subject to debugging)
       apps.push(xrfNew);
       elemOutputResult.textContent = sc + " " + mc;
-      if (sc == "DB" && mc == "D2") {
+      if (sc == "DB" && mc == debug2) {
         // middle of debugging
-        debugState = ["OK", "D1", cnt, apps];
+        debugState = ["OK", debug1, cnt, apps];
         data = XRFtoComb(apps[0]);
       } else {
         // finished debugging
@@ -349,22 +336,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
   elemJSON.disabled = false;
   elemJSON.checked = outputJSON;
+  setOutputJSON(outputJSON);
   elemJSON.addEventListener("click", () => {
     outputJSON = elemJSON.checked;
+    setOutputJSON(outputJSON);
     updateOutput();
   });
 
   elemCXP.disabled = false;
   elemCXP.checked = outputComb;
+  setOutputComb(outputComb);
   elemCXP.addEventListener("click", () => {
     outputComb = elemCXP.checked;
+    setOutputComb(outputComb);
     updateOutput();
   });
 
   elemStr.disabled = false;
   elemStr.checked = outputStr;
+  setOutputStr(outputStr);
   elemStr.addEventListener("click", () => {
     outputStr = elemStr.checked;
+    setOutputStr(outputStr);
     updateOutput();
   });
   elemC.disabled = false;
